394a61975edea336c45fbf48252ec0a724c338e1
vloxomire <vloxomire@gmail.com>
2018-07-31 21:52:18 -0300

    Se crea el Intent para el main2Activity.class

--- app/src/main/java/itmaster/max/bitcoin/ListenerConfirmar.java
+++ app/src/main/java/itmaster/max/bitcoin/ListenerConfirmar.java
@@ -1,5 +1,6 @@
 package itmaster.max.bitcoin;
 
+import android.content.Intent;
 import android.view.View;
 
 public class ListenerConfirmar implements View.OnClickListener {
@@ -9,8 +10,11 @@ public class ListenerConfirmar implements View.OnClickListener {
         this.context = context;
     }
 
+
+
+
     @Override
     public void onClick(View view) {
-        
+        Intent activity2 = new Intent(context, Main2Activity.class );
     }
 }
